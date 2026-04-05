4d007345016b23af159ef69a5242fb7cc3f03077
Deekshasuresh009 <deekshasuresh1976@gmail.com>
2026-04-05 11:48:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Portfolio Loaded Successfully");
+AOS.init();
+
+function openProject(page) {
+  window.location.href = page;
+}
+
+function scrollToProjects() {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Typing Effect
+const text = "Devi Deekshitha";
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 100);
+  }
+}
+
+type();
